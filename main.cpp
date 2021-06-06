b2438d0fbcbfbc1f58b833689f6864e9eaea59cb
Yana <onofriichuck@mail.ru>
2021-06-06 12:01:33 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
+void input(int*, int*)
 int main ()
 {
+setlocale(LC_ALL, "rus");
+int num1, num2;
+input(&num1, &num2);
+cout<<"„исла: "<<num1<<"; "<<num2<<endl;
 return 0;
 }
+void input(int *a, int *b)
+{
+    cout<<"¬ведите первое число ";
+    cin>>*a;
+    cout<<"¬ведите второе число ";
+    cin>>*b;
+    }
